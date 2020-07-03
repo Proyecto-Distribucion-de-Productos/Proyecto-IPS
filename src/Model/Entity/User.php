7eee23ac7ea9e267973c6047fafd8b5fdd63c70b
Marco QM <marcoq558@gmail.com>
2020-07-03 10:59:03 -0500

    Se agrego el hashing para las contraseñas

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Model\Entity;
 
+use Cake\Auth\DefaultPasswordHasher; // Add this line
 use Cake\ORM\Entity;
 
 /**
@@ -49,4 +50,14 @@ class User extends Entity
     protected $_hidden = [
         'password',
     ];
+
+    // Add this method
+    protected function _setPassword($value)
+    {
+        if (strlen($value)) {
+            $hasher = new DefaultPasswordHasher();
+
+            return $hasher->hash($value);
+        }
+    }
 }
