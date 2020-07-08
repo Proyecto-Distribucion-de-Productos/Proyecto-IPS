1b5f2ac71166ba8f258b2b2070d72ee553aa5a61
jcapiavilca <jcapia@unsa.edu.pe>
2020-07-08 15:12:47 -0500

    Se agrego la vista de tablero del administrador

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+/**
+ * Users Controller
+ *
+ * @property \App\Model\Table\UsersTable $Users
+ * @method \App\Model\Entity\User[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class DashboardController extends AppController
+{
+
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+        
+    }
+}
