50eb6e3563b58b37d739bf431fb8a8ff7504c2aa
EvelynMaquera123 <evelynmaquera2901@gmail.com>
2020-07-27 02:54:35 -0500

    css de users roles purchases provinces

--- templates/Admin/Purchases/edit.php
+++ templates/Admin/Purchases/edit.php
@@ -1,35 +1,64 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\Purchase $purchase
- */
-?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $purchase->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $purchase->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Purchases'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Main CSS-->
+  <!--<link rel="stylesheet" type="text/css" href="css/main.css">-->
+  <?= $this->Html->css('main.css') ?>
+  <!-- Font-icon css-->
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>Editar Distrito</title>
+</head>
+<body>
+  <section class="material-half-bg">
+    <div class="cover"></div>
+  </section>
+  <section class="login-content">
+    <div class="login-box">
+      <!-- Inicio del formulario login-->
+      <div class="login-form">
+        <?= $this->Form->create($purchase) ?>
+        <!--<form class="login-form" method="post" accept-charset="utf-8" action="user/login">-->
+          <h3 class="login-head">COMPRA</h3>
+          <div class="form-group">
+            <?php 
+            echo $this->Form->control('provider_id', ['label' => 'PROVEEDOR', 'class' => 'form-control']);
+            echo $this->Form->control('date', ['label' => 'FECHA', 'class' => 'form-control']);
+            echo $this->Form->control('user_id', ['label' => 'USUARIO', 'class' => 'form-control', 'options' => $users]);
+          
+            ?> 
+          </div>
+          <div class="form-group">
+          </div>
+          <div class="form-group btn-container">
+            <button class="btn btn-primary btn-block" type="submit">AGREGAR</button>
+          </div>
+          <!--</form>-->
+          <?= $this->Form->end() ?></div>
         </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="purchases form content">
-            <?= $this->Form->create($purchase) ?>
-            <fieldset>
-                <legend><?= __('Edit Purchase') ?></legend>
-                <?php
-                    echo $this->Form->control('provider_id', ['options' => $providers]);
-                    echo $this->Form->control('date');
-                    echo $this->Form->control('user_id', ['options' => $users]);
-                    echo $this->Form->control('products._ids', ['options' => $products]);
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
-        </div>
-    </div>
-</div>
+      </section>
+      <!-- Essential javascripts for application to work-->
+    <!--<script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>-->
+    <?= $this->Html->script('jquery-3.2.1.min.js') ?>
+    <?= $this->Html->script('popper.min.js') ?>
+    <?= $this->Html->script('bootstrap.min.js') ?>
+    <?= $this->Html->script('main.js') ?>
+
+    <!-- The javascript plugin to display page loading on top-->
+    <!--<script src="js/plugins/pace.min.js"></script>-->
+    <?= $this->Html->script('plugins/pace.min.js') ?>
+
+    <script type="text/javascript">
+      // Login Page Flipbox control
+      $('.login-content [data-toggle="flip"]').click(function() {
+        $('.login-box').toggleClass('flipped');
+        return false;
+      });
+    </script>
+  </body>
+  </html>
--- templates/Admin/Users/edit.php
+++ templates/Admin/Users/edit.php
@@ -1,37 +1,67 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\User $user
- */
-?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $user->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $user->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Users'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Main CSS-->
+  <!--<link rel="stylesheet" type="text/css" href="css/main.css">-->
+  <?= $this->Html->css('main.css') ?>
+  <!-- Font-icon css-->
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>Editar Distrito</title>
+</head>
+<body>
+  <section class="material-half-bg">
+    <div class="cover"></div>
+  </section>
+  <section class="register-content">
+      <div class="register-box">
+        <!-- Inicio del formulario login-->
+        <div class="register-form">
+        <?= $this->Form->create() ?>
+        <!--<form class="register-form" method="post" accept-charset="utf-8" action="user/login">-->
+          <h3 class="register-head"><i class="fa fa-lg fa-fw fa-user"></i>USUARIO</h3>
+          <div class="form-group">
+            <?php 
+            echo $this->Form->control('name', ['label' => 'Nombre', 'class' => 'form-control']);
+            echo $this->Form->control('email', ['label' => 'Correo electrónico', 'class' => 'form-control']);
+            echo $this->Form->control('password', ['label' => 'Contraseña', 'class' => 'form-control']);
+            echo $this->Form->control('firstname', ['label' => 'Primer apellido', 'class' => 'form-control']);
+            echo $this->Form->control('secondname', ['label' => 'Segundo apellido', 'class' => 'form-control']);
+            echo $this->Form->control('role_id', ['label' => 'Rol', 'class' => 'form-control', 'options' => $roles]);
+            ?> 
+          </div>
+          <div class="form-group">
+          </div>
+          <div class="form-group btn-container">
+            <button class="btn btn-primary btn-block" type="submit">AGREGAR</button>
+          </div>
+          <!--</form>-->
+          <?= $this->Form->end() ?></div>
         </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="users form content">
-            <?= $this->Form->create($user) ?>
-            <fieldset>
-                <legend><?= __('Edit User') ?></legend>
-                <?php
-                    echo $this->Form->control('name');
-                    echo $this->Form->control('email');
-                    echo $this->Form->control('password');
-                    echo $this->Form->control('firstname');
-                    echo $this->Form->control('secondname');
-                    echo $this->Form->control('role_id', ['options' => $roles]);
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
-        </div>
-    </div>
-</div>
+      </section>
+      <!-- Essential javascripts for application to work-->
+    <!--<script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>-->
+    <?= $this->Html->script('jquery-3.2.1.min.js') ?>
+    <?= $this->Html->script('popper.min.js') ?>
+    <?= $this->Html->script('bootstrap.min.js') ?>
+    <?= $this->Html->script('main.js') ?>
+
+    <!-- The javascript plugin to display page loading on top-->
+    <!--<script src="js/plugins/pace.min.js"></script>-->
+    <?= $this->Html->script('plugins/pace.min.js') ?>
+
+    <script type="text/javascript">
+      // Login Page Flipbox control
+      $('.login-content [data-toggle="flip"]').click(function() {
+        $('.login-box').toggleClass('flipped');
+        return false;
+      });
+    </script>
+  </body>
+  </html>
+
